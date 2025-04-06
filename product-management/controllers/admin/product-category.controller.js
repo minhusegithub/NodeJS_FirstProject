6dccdfbc6ca836cc9d80b38168cdc9c2e91d152f
minhusegithub <minhmathez@gmail.com>
2025-04-06 16:51:00 +0700

    Xây dựng phần quản lí danh mục và fix lỗi đánh số thứ tự

--- product-management/controllers/admin/product-category.controller.js
+++ product-management/controllers/admin/product-category.controller.js
@@ -3,19 +3,26 @@ const productCategory = require("../../models/product-category.model")
 
 const systemConfig = require("../../config/system");
 
+const createTreeHelper = require("../../helpers/createTree")
+
 //[GET] /admin/products-category
 module.exports.index = async (req , res) => {
    
     let find = {
         deleted: false
     }
+    // Ham de quy de xay dung cay phan cap
+    createTreeHelper.creatTree
+
+
 
     const records = await productCategory.find(find)
-   
+
+    const newRecords = createTreeHelper.tree(records);
 
     res.render("admin/pages/products-category/index" , {
         pageTitle: "Danh mục sản phẩm",
-        records: records
+        records: newRecords
     });
 
 }
@@ -23,11 +30,20 @@ module.exports.index = async (req , res) => {
 //[GET] /admin/products-category/create
 module.exports.create = async (req , res) => {
    
-     
+    let find = {
+        deleted: false
+    }
+    // Ham de quy de xay dung cay phan cap
+    
+    const records = await productCategory.find(find)
+
+    const newRecords = createTreeHelper.tree(records);
+
+    // console.log(newRecords);
 
     res.render("admin/pages/products-category/create" , {
         pageTitle: "Tạo danh mục sản phẩm",
-        
+        records: newRecords
     });
 
 }
