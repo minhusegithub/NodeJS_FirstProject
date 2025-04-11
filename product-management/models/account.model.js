ddf5981a27db474a493bd0b49946937ec20f6d45
minhusegithub <minhmathez@gmail.com>
2025-04-11 21:28:21 +0700

    Tạo collection accounts

---
+++ product-management/models/account.model.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const generate = require("../helpers/generate") 
+
+
+const accountSchema = new mongoose.Schema(
+    {
+        fullName: String,
+        eamail: String,
+        password: String,
+        token:{
+            type: String,
+            default: generate.generateRandomString(20)  
+        },
+        phone: String,
+        avatar: String,
+        role_id: String,
+        status: String,
+        deleted:{
+            type: Boolean,
+            default: false, 
+        },
+        deleteAt: Date,
+    },
+    {
+        timestamps: true  
+    }
+);
+
+const Account = mongoose.model('Account', accountSchema  , "accounts"); 
+module.exports = Account;
+
+
+
+
