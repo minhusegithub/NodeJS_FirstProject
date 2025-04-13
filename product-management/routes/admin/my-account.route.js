7ac0417c0a9205d3bbfcc617cb6cc661661a5b53
minhusegithub <minhmathez@gmail.com>
2025-04-14 01:59:09 +0700

    Trang thông tin cá nhân

---
+++ product-management/routes/admin/my-account.route.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const multer = require("multer");
+const router = express.Router();
+const upload = multer();
+
+
+//khai bao controller
+const controller = require("../../controllers/admin/my-account.controller");
+
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware")
+
+
+//goi controller
+router.get('/', controller.index);
+
+router.get('/edit', controller.edit);
+
+router.patch( 
+    '/edit',
+    upload.single("avatar"),
+    uploadCloud.upload,
+    controller.editPatch
+    
+);
+
+
+
+
+
+
+
+module.exports = router;
