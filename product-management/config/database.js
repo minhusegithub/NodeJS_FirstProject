1316748f6bfc7abddd999d68c654c1a8dd123c2f
minhusegithub <minhmathez@gmail.com>
2025-12-05 02:20:56 +0700

    Thêm logic và làm đẹp tính năng đổi mật khẩu

--- product-management/config/database.js
+++ product-management/config/database.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 module.exports.connect = async ()=>{
     try{
         await mongoose.connect(process.env.MONGO_URL);
-        console.log("Connected !");
+        console.log("Connected to MongoDB successfully!");
     } catch(error){
         console.log("Connect error !");
     }
